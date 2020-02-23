c1d8dccb193d67e9f741c0be0a6bed1e9566b881
gsbalaji20111 <sabalaji@FWS-CHE-LT-3288.local>
2020-02-23 12:56:15 +0530

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.11.1'
 # Use sqlite3 as the database for Active Record
-
+gem 'faker'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -25,6 +25,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '2.3.2.0' 
 gem 'sprockets', '2.11.0'
 gem 'bcrypt-ruby'
+
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,16 @@ def sign_in(user)
     self.current_user = user
 end
 
+
+def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
 def signed_in?
     !current_user.nil?
 end
@@ -18,7 +28,12 @@ end
 def current_user
     remember_token = User.digest(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
-end
+  end
+
+
+def current_user?(user)
+    user == current_user
+  end
 
 def sign_out
     current_user.update_attribute(:remember_token,
